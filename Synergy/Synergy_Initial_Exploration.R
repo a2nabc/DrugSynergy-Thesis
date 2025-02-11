5f6c3e57e6b29be85b8d9a470774abf5a6b272fa
Anna Bernadas <104145634+a2nabc@users.noreply.github.com>
2025-02-10 22:06:34 -0500

    update rds treatment for NCI_ALMANAC

--- Synergy/Synergy_Initial_Exploration.R
+++ Synergy/Synergy_Initial_Exploration.R
@@ -11,16 +11,19 @@ synergy_data <- x@assays$profiles
 
 
 assayindex <- x@.intern$assayIndex
-assayindex <- assayindex[, c("rowKey", ".profiles")]
+assayindex <- assayindex[, c("rowKey", "colKey", ".profiles")]
 
 merged_synergy_rowData <- merge(synergy_data, assayindex, by=".profiles")
+
+cell_line_data <- x@colData[, c("sampleid", "colKey")]
+merged_synergy_cell_lines <- merge(merged_synergy_rowData, cell_line_data, by="colKey")
 
 drug_pairs <- x@rowData %>%
   select(treatment1id, treatment2id, rowKey) %>%
   na.omit()
 
 # Merge with synergy scores
-merged_synergy_data <- merge(merged_synergy_rowData, drug_pairs, by = "rowKey")
+merged_synergy_data <- merge(merged_synergy_cell_lines, drug_pairs, by = "rowKey")
 
 # Count the number of observations for each drug pair
 drug_pair_counts <- merged_synergy_data %>%
@@ -29,3 +32,11 @@ drug_pair_counts <- merged_synergy_data %>%
   arrange(desc(count))
 
 
+
+#######################3 GENE EXPRESSION ##################################
+y<-molecularProfiles(data, "rnaseq.comp")
+
+y <- t(y)
+y <- as.data.frame(y)
+y$sampleid <- rownames(y)
+merged_data_with_y <- merge(merged_synergy_data, y, by = "sampleid")
