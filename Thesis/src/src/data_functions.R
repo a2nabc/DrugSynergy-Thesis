1c2f5a58d27a43004d66d5d9103fb554392926a3
Anna Bernadas <104145634+a2nabc@users.noreply.github.com>
2025-03-23 19:27:34 -0400

    Add files via upload

--- Thesis/src/src/data_functions.R
+++ Thesis/src/src/data_functions.R
@@ -17,6 +17,35 @@ load_data <- function(expression_path, response_path) {
   return(list(expression = expression_data, response = response_data))
 }
 
+create_output_folders <- function(model_types, experiment_positive, results_dir, features_dir, eval_dir, correlations_dir, cv_dir) {
+  results_subdir <- if (experiment_positive == "true") "positive/" else "negative/"
+  dir.create(results_dir, showWarnings = FALSE)
+  dir.create(paste0(results_dir, results_subdir), showWarnings = FALSE)
+  dir.create(paste0(results_dir, results_subdir, "metadata/"), showWarnings = FALSE)
+  for (model in model_types) {
+    dir.create(paste0(results_dir, results_subdir, model), showWarnings = FALSE)
+    dir.create(paste0(results_dir, results_subdir, model, "/",  features_dir), showWarnings = FALSE)
+    dir.create(paste0(results_dir, results_subdir, model, "/", eval_dir), showWarnings = FALSE)
+    dir.create(paste0(results_dir, results_subdir, model, "/", correlations_dir), showWarnings = FALSE)
+    dir.create(paste0(results_dir, results_subdir, model, "/", cv_dir), showWarnings = FALSE)
+  }
+  return(results_subdir)
+}
+
+create_folder_metadata <- function(results_metadata_dir){
+  dir.create(results_metadata_dir, showWarnings = FALSE)
+  data_dimensions <- data.frame(
+    Drug = character(),
+    Model_Type = character(),
+    Training_Samples = integer(),
+    Training_Features = integer(),
+    Testing_Samples = integer(),
+    Testing_Features = integer(),
+    stringsAsFactors = FALSE
+  )
+  return(data_dimensions)
+}
+
 filter_cell_lines <- function(is_consistency_experiment, source_data, target_data) {
   source_cells <- source_data$expression$Cell_line
   target_cells <- target_data$expression$Cell_line
